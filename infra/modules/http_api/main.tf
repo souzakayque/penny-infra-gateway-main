bd2a35eed97bbc8ce8c856527892cb255521a697
Kayque Souza Pereira Lima <kpereir6@gmail.com>
2026-09-13 11:17:15 -0300

    fix statement_id

--- infra/modules/http_api/main.tf
+++ infra/modules/http_api/main.tf
@@ -72,7 +72,16 @@ resource "aws_apigatewayv2_route" "this" {
 resource "aws_lambda_permission" "apigw" {
   for_each = var.routes
 
-  statement_id  = "AllowAPIGatewayInvoke-${regexreplace(each.key, "[^a-zA-Z0-9_-]", "_")}"
+  statement_id = "AllowAPIGatewayInvoke-${replace(
+    replace(
+      replace(
+        replace(each.key, " ", "-"),
+        "/", "_"
+      ),
+      "{", ""
+    ),
+    "}", ""
+  )}"
   action        = "lambda:InvokeFunction"
   function_name = each.value.lambda_function_name
   principal     = "apigateway.amazonaws.com"
